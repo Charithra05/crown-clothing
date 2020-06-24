45d031d2e1b9e635260f44e5ae213b8e38c2520f
Charithra <you@example.com>
2020-06-24 22:20:37 +0530

    Updates

---
+++ precache-manifest.1ee88d51f478dc86c1a86045eacd23b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067e324ae43005fc9f9d4e2bfa5b89fd",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "b9c4106106694203a400",
+    "url": "/crown-clothing/static/css/main.5a321158.chunk.css"
+  },
+  {
+    "revision": "40275d2fabf9c8a5425e",
+    "url": "/crown-clothing/static/js/2.9fb7ad46.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/crown-clothing/static/js/2.9fb7ad46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c4106106694203a400",
+    "url": "/crown-clothing/static/js/main.18b8ffea.chunk.js"
+  },
+  {
+    "revision": "fbd3e5f479ef6956ae55",
+    "url": "/crown-clothing/static/js/runtime-main.ce934caa.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
